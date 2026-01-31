df84a1d9cc437d95ddb0eb90c09408bd3e857c0f
idode kerobo <idode.kerobo@gmail.com>
2026-01-31 14:11:09 -0800

    initial macos app

---
+++ photo-agent-macos/photo-agent-macos/photo_agent_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_agent_macosApp.swift
+//  photo-agent-macos
+//
+//  Created by Idode Kerobo on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct photo_agent_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
